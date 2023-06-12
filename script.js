7638759d756705d8f88a5aec53cdd18525fcc192
dlwjdls <132539078+dlwjdls@users.noreply.github.com>
2023-06-12 20:07:22 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+<script>
+    // JavaScript 코드
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const cartItems = document.getElementById('cart-items');
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (event) => {
+            const itemName = event.target.value;
+
+            if (event.target.checked) {
+                const listItem = document.createElement('li');
+                // const itemImage = document.createElement('img');
+                // itemImage.src = `${itemName}.jpg`;
+                // itemImage.alt = itemName;
+                // listItem.appendChild(itemImage);
+                listItem.appendChild(document.createTextNode(itemName));
+                cartItems.appendChild(listItem);
+            } else {
+                const items = cartItems.getElementsByTagName('li');
+                for (let i = 0; i < items.length; i++) {
+                    if (items[i].textContent === itemName) {
+                        cartItems.removeChild(items[i]);
+                        break;
+                    }
+                }
+            }
+        });
+    });
+</script>
